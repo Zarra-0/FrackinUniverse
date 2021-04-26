20b3e92dd993f48b1c7098afe5956467effcc344
Zarra-0 <72447426+Zarra-0@users.noreply.github.com>
2021-04-26 14:26:51 +0100

    Vanilla racial armour recipes
    Dropped as rare loot in the relevant (vanilla) missions, with terrible drop rates in the regular missions and good droprates in the t10 variants.
    
    Novakid blueprints are dropped by Sunset Rider outlaws, including in Novakid Asteroids, and have somewhat buffed chances in T6 systems.

---
+++ items/active/randomblueprint/furacialblueprint.lua
@@ -0,0 +1,38 @@
+function init()
+  self.recipes = config.getParameter("recipes")
+  self.swingTime = config.getParameter("swingTime")
+  activeItem.setArmAngle(-math.pi / 2)
+end
+
+function update(dt, fireMode, shiftHeld)
+  updateAim()
+
+  if not self.swingTimer and fireMode == "primary" and player then
+    self.swingTimer = self.swingTime
+  end
+
+  if self.swingTimer then
+    self.swingTimer = math.max(0, self.swingTimer - dt)
+
+    activeItem.setArmAngle((-math.pi / 2) * (self.swingTimer / self.swingTime))
+
+    if self.swingTimer == 0 then
+      learnBlueprint()
+    end
+  end
+end
+
+function learnBlueprint()
+  for _,recipe in pairs(self.recipes) do
+    player.giveBlueprint(recipe)
+  end
+
+  animator.playSound("learnBlueprint")
+
+  item.consume(1)
+end
+
+function updateAim()
+  self.aimAngle, self.aimDirection = activeItem.aimAngleAndDirection(0, activeItem.ownerAimPosition())
+  activeItem.setFacingDirection(self.aimDirection)
+end
