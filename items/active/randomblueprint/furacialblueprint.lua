002919c967a24f26f0ad74a8a89ac6730ecf3771
Zarra-0 <72447426+Zarra-0@users.noreply.github.com>
2021-05-18 19:33:17 +0100

    Update rotation angle

--- items/active/randomblueprint/furacialblueprint.lua
+++ items/active/randomblueprint/furacialblueprint.lua
@@ -8,28 +8,44 @@ function update(dt, fireMode, shiftHeld)
   updateAim()
 
   if not self.swingTimer and fireMode == "primary" and player then
+    checkRecipes()
     self.swingTimer = self.swingTime
   end
 
-  if self.swingTimer then
-    self.swingTimer = math.max(0, self.swingTimer - dt)
+  if self.recipePass then
+    if self.swingTimer then
+      self.swingTimer = math.max(0, self.swingTimer - dt)
 
-    activeItem.setArmAngle((-math.pi / 2) * (self.swingTimer / self.swingTime))
+      activeItem.setArmAngle((-math.pi / 2) * (self.swingTimer / self.swingTime))
 
-    if self.swingTimer == 0 then
-      learnBlueprint()
+      if self.swingTimer == 0 then
+        learnBlueprint()
+      end
     end
   end
 end
 
 function learnBlueprint()
-  for _,recipe in pairs(self.recipes) do
-    player.giveBlueprint(recipe)
+  if checkRecipes() then
+    for _,recipe in pairs(self.recipes) do
+      player.giveBlueprint(recipe)
+    end
+    animator.playSound("learnBlueprint")
+    item.consume(1)
+  else
+    return
   end
+end
 
-  animator.playSound("learnBlueprint")
-
-  item.consume(1)
+function checkRecipes()
+  for _,itemName in pairs(self.recipes) do
+    if not player.blueprintKnown(itemName) then
+      self.recipePass = true
+      return true
+    end
+  end
+  self.recipePass = false
+  return false
 end
 
 function updateAim()
