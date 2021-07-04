1e7ee662a11ce87ed67dd231a0648f9cefc5c135
Zarra-0 <72447426+Zarra-0@users.noreply.github.com>
2021-07-04 11:22:58 +0100

    Vanilla racial blueprints
    Now ready for release

--- items/active/randomblueprint/furacialblueprint.lua
+++ items/active/randomblueprint/furacialblueprint.lua
@@ -1,16 +1,12 @@
 function init()
   self.recipes = config.getParameter("recipes")
   self.swingTime = config.getParameter("swingTime")
+  self.failMessage = config.getParameter("failMessage")
   activeItem.setArmAngle(-math.pi / 2)
 end
 
 function update(dt, fireMode, shiftHeld)
   updateAim()
-
-  if not self.swingTimer and fireMode == "primary" and player then
-    checkRecipes()
-    self.swingTimer = self.swingTime
-  end
 
   if self.recipePass then
     if self.swingTimer then
@@ -25,27 +21,33 @@ function update(dt, fireMode, shiftHeld)
   end
 end
 
-function learnBlueprint()
-  if checkRecipes() then
-    for _,recipe in pairs(self.recipes) do
-      player.giveBlueprint(recipe)
-    end
-    animator.playSound("learnBlueprint")
-    item.consume(1)
-  else
-    return
+function activate(fireMode, shiftHeld)
+  if not self.swingTimer and player then
+    checkRecipes()
+    self.swingTimer = self.swingTime
   end
+
+  if not self.recipePass and player then
+    player.interact("ShowPopup", {message = self.failMessage})
+  end
+end
+
+function learnBlueprint()
+  for _,recipe in pairs(self.recipes) do
+    player.giveBlueprint(recipe)
+  end
+  animator.playSound("learnBlueprint")
+  item.consume(1)
 end
 
 function checkRecipes()
   for _,itemName in pairs(self.recipes) do
     if not player.blueprintKnown(itemName) then
       self.recipePass = true
-      return true
+      return
     end
   end
   self.recipePass = false
-  return false
 end
 
 function updateAim()
